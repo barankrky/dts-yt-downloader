fd946f7e233f14b1fc7cab8e2b6de2286bc1cbcf
Baran Karakaya <thebarankarakaya@gmail.com>
2024-03-28 01:10:53 +0300

    Update mainwindow.py

--- ui/mainwindow_ui.py
+++ ui/mainwindow_ui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/home/burakdmrbkr/dts-yt-downloader/ui/mainwindow.ui'
+# Form implementation generated from reading ui file '/home/barankrky/dts-yt-downloader/ui/mainwindow.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
@@ -14,14 +14,18 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(698, 256)
+        MainWindow.resize(698, 220)
         font = QtGui.QFont()
         font.setFamily("Sans")
         MainWindow.setFont(font)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.appLabel = QtWidgets.QLabel(self.centralwidget)
-        self.appLabel.setGeometry(QtCore.QRect(0, 0, 701, 41))
+        self.appLabel.setGeometry(QtCore.QRect(0, 10, 701, 41))
+        font = QtGui.QFont()
+        font.setFamily("DejaVu Sans")
+        font.setPointSize(12)
+        self.appLabel.setFont(font)
         self.appLabel.setAlignment(QtCore.Qt.AlignCenter)
         self.appLabel.setObjectName("appLabel")
         self.downloadButton = QtWidgets.QPushButton(self.centralwidget)
@@ -35,17 +39,18 @@ class Ui_MainWindow(object):
         self.urlText.setToolTipDuration(-1)
         self.urlText.setObjectName("urlText")
         self.about = QtWidgets.QPushButton(self.centralwidget)
-        self.about.setGeometry(QtCore.QRect(600, 200, 89, 25))
+        self.about.setGeometry(QtCore.QRect(600, 160, 89, 25))
         self.about.setObjectName("about")
         self.reportButton = QtWidgets.QPushButton(self.centralwidget)
-        self.reportButton.setGeometry(QtCore.QRect(10, 200, 89, 25))
+        self.reportButton.setGeometry(QtCore.QRect(10, 160, 89, 25))
         self.reportButton.setObjectName("reportButton")
         self.infoText = QtWidgets.QLabel(self.centralwidget)
-        self.infoText.setGeometry(QtCore.QRect(30, 150, 641, 41))
+        self.infoText.setGeometry(QtCore.QRect(20, 100, 661, 41))
         font = QtGui.QFont()
         font.setFamily("DejaVu Sans")
         font.setPointSize(9)
         self.infoText.setFont(font)
+        self.infoText.setStyleSheet("color: rgb(85, 87, 83)")
         self.infoText.setWordWrap(True)
         self.infoText.setObjectName("infoText")
         MainWindow.setCentralWidget(self.centralwidget)
